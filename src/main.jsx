7642b38ba3717f69c32854a746f983dadabd961d
Ahmed-Aloush-2004 <156807632+Ahmed-Aloush-2004@users.noreply.github.com>
2024-03-07 09:09:11 +0300

    Adding file and gitignore

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+// import "./main.css"
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+{/* <App /> */}
+// 
+root.render(
+<>
+  <App/>
+</>
+);
